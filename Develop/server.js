e83fe22d9da6ab686a79d759f095656460a39900
Matt Moncayo <matthew.moncayo@gmail.com>
2020-06-11 23:37:02 -0700

    established dependencies, express config, server map routes, and listeners in the server js file

---
+++ Develop/server.js
@@ -0,0 +1,33 @@
+// DEPENDENCIES
+// ================================================================================
+
+// establishes the npm packages we will need to use to provide functionality to our code
+var express = require("express");
+
+// EXPRESS CONFIGURATION
+// ================================================================================
+
+// creates the express server
+var app = express();
+
+// sets port to be used in our listener
+var PORT = process.env.PORT || 3000;
+
+// allows express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// ================================================================================
+
+// these routes direct our server to particular files that will provide instructions on how to respond or request data 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// LISTENER
+// ================================================================================
+
+// initiates the start of our code when we run node
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
