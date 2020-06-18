ed549fcb8296dab2e534b03a3c88b49ad51bc07e
Matt Moncayo <matthew.moncayo@gmail.com>
2020-06-18 02:25:21 -0700

    fixed errors with updated read files once note was deleted

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,12 +2,16 @@
 // ================================================================================
 // establishes the package needed to read and write the notes entered
 const fs = require("fs");
+const path = require("path");
+const util = require('util');
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 // LOAD DATA
 // ===============================================================================
 
-// this is where our saved notes contain its information in the array
-var savedNotes = [];
+// where the database for notes is stored
+const dbFilePath = path.join(__dirname, '../db/db.json');
 
 // ROUTING
 // ===============================================================================
@@ -18,21 +22,21 @@ module.exports = function (app) {
     // ---------------------------------------------------------------------------
 
     // this reads the json file and returns all of the saved notes as json 
-    app.get("/api/notes", function (err, res) {
-        try {
-            // reads in db.json file (the notes saved in the json file)
-            savedNotes = fs.readFileSync("./db/db.json", "utf-8");
-
-            // parses it so the saved notes are an array of objects
-            savedNotes = JSON.parse(savedNotes);
-        }
-        // takes care of error handling
-        catch (err) {
-            console.log(err);
-        }
-        // responds by sending the object to the browser
-        res.json(savedNotes);
+    app.get("/api/notes", function (req, res) {
+        readFile(dbFilePath, "utf8")
+            .then(data => res.json(JSON.parse(data)));
     });
+
+    // generates unique ID for every saved note
+    function newID(currentNotes) {
+        let highestId = 0;
+        currentNotes.forEach(note => {
+            if (note.id > highestId) {
+                highestId = note.id;
+            }
+        });
+        return highestId + 1;
+    }
 
     // API POST Requests
     // -----------------------------------------------------------------------------
@@ -40,37 +44,16 @@ module.exports = function (app) {
     // this receives the new note to save on the request body, then adds it to the db.json file (saved notes), 
     // and returns the new note to the client
     app.post("/api/notes", function (req, res) {
-        try {
-            // reads the saved notes file (db.json)
-            savedNotes = fs.readFileSync("./db/db.json", "utf-8");
-
-            // this parses the date to save as an array of objects
-            savedNotes = JSON.parse(savedNotes);
-
-            // this sets a unique id to the saved notes by using its index as the id number
-            req.body.id = savedNotes.length;
-
-            // adds the newly written note into the saved notes file (db.json) as an array of note objects 
-            // (the user input is the req.body) 
-            savedNotes.push(req.body);
-
-            // stringify the note so it can be written to the file
-            savedNotes = JSON.stringify(savedNotes);
-
-            // now you can write the new note into the saved notes file
-            fs.writeFileSync("./db/db.json", savedNotes, "utf-8", (err) => {
-                // takes care of error handling
-                if (err) throw err;
+        readFile(dbFilePath, "utf8")
+            .then(data => {
+                let notes = JSON.parse(data);
+                const newNote = { ...req.body, "id": newID(notes) };
+                notes.push(newNote);
+                writeFile(dbFilePath, JSON.stringify(notes))
+                    .then(() => {
+                        res.json(newNote);
+                    });
             });
-            // once it is written, change back to an array of objects and send it back to the browser
-            res.json(JSON.parse(savedNotes));
-            console.log(savedNotes);
-
-        }
-        // more error handling
-        catch (err) {
-            throw err;
-        }
     });
 
     // API DELETE Requests
@@ -79,30 +62,15 @@ module.exports = function (app) {
     // this receives the saved note with the unique id that is to be deleted, then reads all the notes from db.json, 
     // removes the note with the right id, and finally re-writes the notes ot the db.json file (saved notes)
     app.delete("/api/notes/:id", function (req, res) {
-        try {
-            // reads the saved notes file (db.json)
-            savedNotes = fs.readFileSync("./db/db.json", "utf-8");
-
-            // this parses the date to save as an array of objects
-            savedNotes = JSON.parse(savedNotes);
-
-            // delete the saved note from the array of note objects by creating a remove method
-            // the remove method used is an array filter that returns elements that do not match the id of the note to be deleted
-            savedNotes = savedNotes.filter((note) => {
-                return note.id != req.params.id;
-            });
-            // stringify the updated saved notes so they can be written to the db.json file
-            savedNotes = JSON.stringify(savedNotes);
-
-            // now you can re-write the saved notes back into the saved notes file updated without the deleted note
-            fs.writeFileSync("./db/db.json", savedNotes, "utf-8", (err) => {
-                // takes care of error handling
-                if (err) throw err;
-            });
-        }
-        // more error handling
-        catch (err) {
-            throw err;
-        }
+        const idToBeDeleted = parseInt(req.params.id);
+        readFile(dbFilePath, "utf8")
+          .then(data => {
+            let notes = JSON.parse(data);
+            notes = notes.filter(newNote => newNote.id !== idToBeDeleted);
+            writeFile(dbFilePath, JSON.stringify(notes))
+              .then(() => {
+                res.send('A request to delete note has been submitted.');
+              });
+          });
     });
 };
